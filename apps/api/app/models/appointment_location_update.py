fa79324683773b8a5767c0fe547befcd2faa6c6e
Anthony Wright <anthony@mrwrite.dev>
2025-12-31 15:23:21 -0600

    Update appointment_location_update.py

--- apps/api/app/models/appointment_location_update.py
+++ apps/api/app/models/appointment_location_update.py
@@ -12,7 +12,7 @@ class AppointmentLocationUpdate(Base):
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     appointment_id = Column(UUID(as_uuid=True), ForeignKey("appointments.id"), nullable=False)
-    company_user_id = Column(UUID(as_uuid=True), ForeignKey("company_users.user_id"), nullable=False)
+    user_id = Column(UUID(as_uuid=True), ForeignKey("company_users.user_id"), nullable=False)
     lat = Column(Float, nullable=False)
     lng = Column(Float, nullable=False)
     heading = Column(Float, nullable=True)
